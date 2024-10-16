3703c269462fc81d0562ed9d07268c7c9e3a5188
Lucas <lurvetti@gmail.com>
2024-10-16 12:02:48 -0300

    changes

--- pages/db_editor.py
+++ pages/db_editor.py
@@ -3,26 +3,13 @@ from streamlit_ace import st_ace
 
 st.set_page_config(layout="wide")
 
-file_path = 'pages/JUN92d.bs'  # Replace with the pa
+file_path = 'databases/JUN92d.bs'  # Replace with the pa
 with open(file_path, 'r') as file:
     db_value = file.read()
-
-# Example usage
-# file_path = 'tcds55_p07_cord.bs'  # Replace with the path to your .bs file
-file_path = 'databases/JUN92d.bs'  # Replace with the path to your .bs file
-
-
-# Example usage
-# file_path = 'tcds55_p07_cord.bs'  # Replace with the path to your .bs file
-
-
 
 c1, c2 = st.columns([1,3])
 
 with c1:
     st.write("### Edit your database!")
 with c2:
-# Spawn a new Ace editor
     st_ace(value=db_value,theme='dracula',height=500)
-
-# Display editor's content as you type
