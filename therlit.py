e2b9e03a115760718460ec0a8076988f5ea8d09e
Lucas <lurvetti@gmail.com>
2025-02-15 19:20:09 -0300

    back

--- therlit.py
+++ therlit.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
 def main():
+    
+    # setup pages
     home = st.Page("pagination/home.py")
     runner_page = st.Page("theriak_runner/runner.py")
     db_catalog_page = st.Page("pagination/db_catalog.py")
 
+    # setup navigation and run
     pg = st.navigation([home,runner_page,db_catalog_page])
     pg.run()
 
